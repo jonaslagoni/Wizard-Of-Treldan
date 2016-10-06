4dbe5e12e6b8ce6c412f12e961bb45254555b7b3
jonaslagoni <jonas-lt@live.dk>
2016-10-06 11:24:25 +0200

    added items

--- zuul-framework/src/TWoT/Item.java
+++ zuul-framework/src/TWoT/Item.java
@@ -7,8 +7,41 @@ package TWoT;
 
 /**
  *
- * @author Lagoni
+ * @author Mads
  */
 public class Item {
     
+    // creating variables for Items
+    private String ITEMNAME;
+    private int VALUE;
+    private String DESCRIPTION;
+    
+    // creating the Constructor
+    public Item(String ITEMNAME, int VALUE, String DESCRIPTION) {
+        this.DESCRIPTION = DESCRIPTION;
+        this.ITEMNAME = ITEMNAME;
+        this.VALUE = VALUE;
+        
+    }
+
+    /**
+     * @return the ITEMNAME
+     */
+    public String getITEMNAME() {
+        return ITEMNAME;
+    }
+
+    /**
+     * @return the VALUE
+     */
+    public int getVALUE() {
+        return VALUE;
+    }
+
+    /**
+     * @return the DESCRIPTION
+     */
+    public String getDESCRIPTION() {
+        return DESCRIPTION;
+    }
 }
