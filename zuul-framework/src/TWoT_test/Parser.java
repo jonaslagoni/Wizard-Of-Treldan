42777a2000b5f3da85b630049ec1101ad19ef048
jonaslagoni <jonas-lt@live.dk>
2016-11-02 13:51:04 +0100

    mod parser

--- zuul-framework/src/TWoT_test/Parser.java
+++ zuul-framework/src/TWoT_test/Parser.java
@@ -34,8 +34,11 @@ public class Parser{
         if(tokenizer.hasNext()) {
             word1 = tokenizer.next().toLowerCase();
             // If the user has entered 2'n word set word2 to the 2'n word.
-            while(tokenizer.hasNext()){
-                word2 += " " + tokenizer.next().toLowerCase();
+            if(tokenizer.hasNext()){
+                word2 = tokenizer.next().toLowerCase();
+            }
+            if(tokenizer.hasNext()){
+                word2 +=  " " + tokenizer.next().toLowerCase();
             }
         }
         // Return the Command object.
