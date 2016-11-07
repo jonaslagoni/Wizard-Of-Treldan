cc165a58940c2c6a457d8bb2a65ed1f80e0f0c4d
mathn16 <mathias626@hotmail.com>
2016-11-07 10:58:16 +0100

    Barrelroll

---
+++ zuul-framework/src/Gui2D/SpriteController/SingleSprite/SingleSpriteGrouping/misc/Barrel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gui2D.SpriteController.SingleSprite.SingleSpriteGrouping.misc;
+
+import Gui2D.SpriteController.SingleSprite.SingleSpriteGrouping.Abstract_sprite;
+
+/**
+ *
+ * @author jonas
+ */
+public class Barrel  extends Abstract_sprite{
+    /**
+     * Sets the Candle sprite
+     */
+    public void setSprite(){
+        super.getSprite().setSpritePosX(828);
+        super.getSprite().setSpritePosY(10);
+        super.getSprite().setSprite_width(6);
+        super.getSprite().setSprite_height(25);
+        super.getSprite().setWidth(6);
+        super.getSprite().setHeight(25);
+    }
+}
