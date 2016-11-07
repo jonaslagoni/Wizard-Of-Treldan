bbe0ed0bcc3c4d8a0d008e5a045409e066d881ea
mathn16 <mathias626@hotmail.com>
2016-11-07 09:27:06 +0100

    House2

---
+++ zuul-framework/src/Gui2D/SpriteController/Maps/House2_sprites.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gui2D.SpriteController.Maps;
+
+import Gui2D.SpriteController.GroupSprites.Groupsprite_circle;
+import Gui2D.SpriteController.GroupSprites.Groupsprite_random;
+import Gui2D.SpriteController.SingleSprite.SingleSprite;
+import Gui2D.SpriteController.Sprite;
+import Gui2D.SpriteController.SpriteController;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author jonas
+ */
+public class House2_sprites{
+    private SpriteController world;
+    
+    
+    public House2_sprites(SpriteController world){
+        this.world = world;
+    }
+    
+    public void setCellar_background_SingleSprites(){
+        
+    }
+
+    /**
+     * @return the cellar_background_sprites
+     */
+    
+}
