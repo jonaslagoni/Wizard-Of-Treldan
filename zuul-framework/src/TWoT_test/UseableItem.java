7a8d73415ddf060fe35fbbde423e81ee7703b224
mathn16 <mathias626@hotmail.com>
2016-11-09 15:06:01 +0100

    Kappa2

---
+++ zuul-framework/src/TWoT_test/UseableItem.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TWoT_test;
+
+/**
+ *
+ * @author Lagoni
+ */
+public class UseableItem extends Item{
+    
+    private int healthRegen;
+    private Usables usables;
+    
+    /**
+     * 
+     * @param itemName
+     * @param itemValue
+     * @param itemDescription 
+     * @param roomDescription 
+     * @param itemId 
+     * @param usables 
+     */
+    public UseableItem(String itemName, int itemValue, String itemDescription, String roomDescription, int itemId, Usables usables, int healthRegen){
+        super.setItemName(itemName);
+        super.setItemValue(itemValue);
+        super.setItemDescription(itemDescription);
+        super.setRoomDescription(roomDescription);
+        super.setItemId(itemId);
+        this.usables= usables;
+        this.healthRegen = healthRegen;
+    }
+
+    /**
+     * @return the healthRegen
+     */
+    public int getHealthRegen() {
+        return healthRegen;
+    }
+
+    /**
+     * @param healthRegen the healthRegen to set
+     */
+    public void setHealthRegen(int healthRegen) {
+        this.healthRegen = healthRegen;
+    }
+    
+    public enum Usables {
+        FOOD,MAGIC_ITEM,ITEM_USE,RARE;
+    }
+    
+    public Usables getUsables() {
+        return usables;
+    }
+
+    public void setUsables(Usables usables) {
+        this.usables = usables;
+    }
+}
