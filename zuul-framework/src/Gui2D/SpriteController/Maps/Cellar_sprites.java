bfb273b1bc5bc73e9bf05cd4ca8a617d33382dcf
jonaslagoni <jonas-lt@live.dk>
2016-11-10 16:28:35 +0100

    added bed and else

--- zuul-framework/src/Gui2D/SpriteController/Maps/Cellar_sprites.java
+++ zuul-framework/src/Gui2D/SpriteController/Maps/Cellar_sprites.java
@@ -72,6 +72,20 @@ public class Cellar_sprites{
             Sprite door = world.getMisc_sprites().getDoor_metal();
             door.setPosition(330, 20);
             cellar_background_sprites.add(door);
+
+            Sprite wood_bed = world.getMisc_sprites().getBed_tree_facing_down();
+            wood_bed.setPosition(430, 64);
+            cellar_background_sprites.add(wood_bed);
+            
+            
+            Sprite skeleton_hanging = world.getMisc_sprites().getSkeleton_hanging();
+            skeleton_hanging.setPosition(390, 15);
+            cellar_background_sprites.add(skeleton_hanging);
+            
+            Sprite cell_bar_straight = new Groupsprite_random(world.getMisc_sprites().getCell_ironbar_straight());
+            cell_bar_straight.setSize(6, 62*2);
+            cell_bar_straight.setPosition(380, 12);
+            cellar_background_sprites.add(cell_bar_straight);
             
     }
 
