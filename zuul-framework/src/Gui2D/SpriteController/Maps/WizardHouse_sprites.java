d80dfa99d9f170183774c2c37915ea46656dd3da
MemmoSJ <Mads28@live.dk>
2016-11-16 13:34:48 +0100

    wiz

--- zuul-framework/src/Gui2D/SpriteController/Maps/WizardHouse_sprites.java
+++ zuul-framework/src/Gui2D/SpriteController/Maps/WizardHouse_sprites.java
@@ -45,57 +45,67 @@ public class WizardHouse_sprites{
         
         Sprite wizardBed = world.getMisc_sprites().getNice_Bed();
         wizardBed.setSize(48, 81);
-        wizardBed.setPosition(250, 200);
+        wizardBed.setPosition(720, 300);
         spriteList.add(wizardBed);
         
         Sprite door = world.getMisc_sprites().getDoor_wood();
         door.setSize(32, 48);
-        door.setPosition(150, 300);
+        door.setPosition(512, 80);
         spriteList.add(door);
         
         Sprite chest = world.getMisc_sprites().getChestClosed();
         chest.setSize(32, 32);
-        chest.setPosition(300, 400);
+        chest.setPosition(736, 120);
         spriteList.add(chest);
         
         Sprite alchemyTable = world.getMisc_sprites().getAlchemy_Table();
         alchemyTable.setSize(64, 60);
-        alchemyTable.setPosition(200, 100);
+        alchemyTable.setPosition(265, 100);
         spriteList.add(alchemyTable);
         
         Sprite treeBundle = world.getMisc_sprites().getTree_Bundle();
         treeBundle.setSize(52, 55);
-        treeBundle.setPosition(500, 200);
+        treeBundle.setPosition(259, 322);
         spriteList.add(treeBundle);
         
         Sprite sacks = world.getMisc_sprites().getSacks_1();
         sacks.setSize(64, 63);
-        sacks.setPosition(300, 100);
+        sacks.setPosition(315, 318);
         spriteList.add(sacks);
+        
+        Sprite bookshelf7 = world.getMisc_sprites().getBookshelf_1();
+        bookshelf7.setSize(64, 69);
+        bookshelf7.setPosition(663, 75);
+        spriteList.add(bookshelf7);
+        
+        Sprite bookshelf6 = world.getMisc_sprites().getBookshelf_1();
+        bookshelf6.setSize(64, 69);
+        bookshelf6.setPosition(628, 75);
+        spriteList.add(bookshelf6);
         
         Sprite bookshelf5 = world.getMisc_sprites().getBookshelf_1();
         bookshelf5.setSize(64, 69);
-        bookshelf5.setPosition(250, 100);
+        bookshelf5.setPosition(593, 75);
         spriteList.add(bookshelf5);
         
         Sprite bookshelf4 = world.getMisc_sprites().getBookshelf_1();
         bookshelf4.setSize(64, 69);
-        bookshelf4.setPosition(250, 100);
+        bookshelf4.setPosition(558, 75);
         spriteList.add(bookshelf4);
         
         Sprite bookshelf3 = world.getMisc_sprites().getBookshelf_1();
         bookshelf3.setSize(64, 69);
-        bookshelf3.setPosition(250, 100);
+        bookshelf3.setPosition(435, 75);
         spriteList.add(bookshelf3);
         
         Sprite bookshelf2 = world.getMisc_sprites().getBookshelf_1();
         bookshelf2.setSize(64, 69);
-        bookshelf2.setPosition(250, 100);
+        bookshelf2.setPosition(400, 75);
         spriteList.add(bookshelf2);
         
         Sprite bookshelf1 = world.getMisc_sprites().getBookshelf_1();
         bookshelf1.setSize(64, 69);
-        bookshelf1.setPosition(250, 100);
+        bookshelf1.setPosition(365, 75);
         spriteList.add(bookshelf1);
         
         // END OF MISC ITEMS
