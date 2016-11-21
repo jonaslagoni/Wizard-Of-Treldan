f02ab4ef612a2a13b497f04c1ed2a27a54cbe269
MemmoSJ <Mads28@live.dk>
2016-11-21 17:01:38 +0100

    Base Dungeon map created

--- zuul-framework/src/Gui2D/Maps/Dungeon.java
+++ zuul-framework/src/Gui2D/Maps/Dungeon.java
@@ -6,20 +6,33 @@
 package Gui2D.Maps;
 
 import Gui2D.SpriteController.Maps.Dungeon_sprites;
-import Gui2D.SpriteController.Maps.Forest_sprites;
+import Gui2D.SpriteController.SingleSprite.PlayerSprite;
 import Gui2D.SpriteController.Sprite;
 import Gui2D.SpriteController.SpriteController;
-import TWoT_test.TWoT;
+import Gui2D.WizardOfTreldan;
+import TWoT_A1.Command;
+import TWoT_A1.CommandWord;
+import TWoT_A1.TWoT;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 /**
  *
- * @author Mads
+ * @author jonas
  */
 public class Dungeon extends Map{
     
@@ -29,15 +42,17 @@ public class Dungeon extends Map{
     // ArrayList for menu key strokes.
     private ArrayList<String> menu_input;
     
-    private Dungeon_sprites dungeon_sprites;
+    private TWoT game;
     
+    private Dungeon_sprites dungeon_sprites;
     /**
-     * Constructor for Cellar
+     * Constructor for Dungeon
      * @param world 
      */
     public Dungeon(SpriteController world){
         //init our super constructor
         super();
+        
         //set the ArrayList's from the super class Map
         super.setInput(new ArrayList<String>());
         super.setMenu_input(new ArrayList<String>());
@@ -46,26 +61,194 @@ public class Dungeon extends Map{
         input = super.getInput();
         menu_input = super.getMenu_input();
         
-        //set the world constructor
-        super.setWorld(world);
-        
         dungeon_sprites = new Dungeon_sprites(world);
         dungeon_sprites.setDungeon_background_SingleSprites();
     }
     
     @Override
     public Scene getScene(){
+        this.game = WizardOfTreldan.getGame();
         Group root = new Group();
         Scene theScene = new Scene( root );
+        Canvas canvas_background = new Canvas(1024, 512);
+        Canvas canvas_foreground = new Canvas(1024, 512);
         theScene.setFill(Color.rgb(83, 83, 83));
-        Canvas background = new Canvas(1024, 512);
-        root.getChildren().add(background);
-        GraphicsContext backgroundContext = background.getGraphicsContext2D();
-        List<Sprite> spriteList = dungeon_sprites.getSpriteList();
+        //set the styleScheet
+        theScene.getStylesheets().add("TextAreaStyle.css");
         
-        for(Sprite s : spriteList) {
-            s.render(backgroundContext);
+        
+        root.getChildren().add(canvas_background);
+        
+        //add a canvas only for the player
+        Canvas player_canvas = new Canvas(1024, 512);
+        //add the canvas to the group
+        root.getChildren().add(player_canvas);
+
+        /**
+         * TextArea used to give the user more information about the game. What
+         * to do and and what happens.
+         */
+        TextArea infobox = Infobox.getInfoBox();
+        //adding stackPane with the textarea component.
+        StackPane s = new StackPane(infobox);
+        s.setPrefSize(300, 150);
+        s.relocate(0, 362);
+        root.getChildren().add(s);
+        //get some of the games welcome message and add to the infobox
+        HashMap<Integer, String> welcome = game.getWelcomeMessages();
+        infobox.appendText(welcome.get("getRooms") + "\n");
+        
+        //Menu testing start
+        PlayerInventory playerinventory = new PlayerInventory(game, infobox);
+        AnchorPane menu = playerinventory.getMenu();
+        //menu testing done
+        
+        //get our player from super class since no inheritence in AnimationTimer
+        PlayerSprite player = super.getPlayer();
+        player.setPosition(500, 400);
+
+        //set the keylisteners to the scene.
+        theScene.setOnKeyReleased(getOnKeyRelease(player));
+        theScene.setOnKeyPressed(getOnKeyPress());
+
+        //create GraphicsContext from our player_canvas
+        GraphicsContext moveable_gc = player_canvas.getGraphicsContext2D();
+        //create GraphicsContext from our canvas_background
+        GraphicsContext dungeon_background = canvas_background.getGraphicsContext2D();
+        GraphicsContext dungeon_foreground = canvas_foreground.getGraphicsContext2D();
+        
+        //get all the sprites used in the dungeon
+        List<Sprite> dungeon_background_sprites = dungeon_sprites.getDungeon_background_sprites();
+        List<Sprite> dungeon_foreground_sprites = dungeon_sprites.getDungeon_foreground_sprites();
+        //render all the sprites
+        for (Sprite sprite : dungeon_background_sprites) {
+            sprite.render(dungeon_background);
         }
+        
+        for (Sprite sprite : dungeon_foreground_sprites) {
+            sprite.render(dungeon_foreground);
+        }
+        
+        //set our world boundaries
+        Rectangle2D worldBoundRight = new Rectangle2D(766, 0, 1, 512);
+        Rectangle2D worldBoundLeft = new Rectangle2D(230, 0, 1, 512);
+        Rectangle2D worldBoundBottom = new Rectangle2D(0, 410, 1024, 1);
+        Rectangle2D worldBoundTop = new Rectangle2D(0, 0, 1024, 1);
+        
+        new AnimationTimer() {
+            //set the current time we started.
+            private long lastNanoTime = System.nanoTime();
+
+            //what to do each cycle
+            @Override
+            public void handle(long currentNanoTime) {
+                //request the focus back
+                root.requestFocus();
+                //get how many sec have passed
+                double elapsedTime = (currentNanoTime - lastNanoTime) / 1000000000.0;
+                //set the lastNanoTime to the nano time from parameter
+                lastNanoTime = currentNanoTime;
+
+                //set our initial direction standstill
+                player.setDirection(PlayerSprite.Direction.STANDSTILL);
+                //now check for the users input
+                //check if the user wants to walk left.
+                if (input.contains("LEFT")) {
+                    //check if the user walks into a world boundary
+                    if (player.intersects_left(worldBoundLeft)) {
+                        //Reset the velocity
+                        player.setVelocity(0, 0);
+                        //check if the player walks into a sprite
+                    }
+                    else {
+                        player.setVelocity(-100, 0);
+                    }
+                    //set the direction the player walks
+                    player.setDirection(PlayerSprite.Direction.WALK_LEFT);
+                }
+                
+                //check if the user wants to walk right.
+                if (input.contains("RIGHT")) {
+                    //check if the user walks into a world boundary
+                    if (player.intersects_right(worldBoundRight)) {
+                        //Reset the velocity
+                        player.setVelocity(0, 0);
+                    //check if the player walks into a sprite
+                    }
+                    else {
+                        player.setVelocity(100, 0);
+                    }
+                    //set the direction the player walks
+                    player.setDirection(PlayerSprite.Direction.WALK_RIGHT);
+                }
+                
+                //check if the user wants to walk up.
+                if (input.contains("UP")) {
+                    //check if the user walks into a world boundary
+                    if (player.intersects_top(worldBoundTop)) {
+                        //Reset the velocity
+                        player.setVelocity(0, 0);
+                        //check if the player walks into a sprite
+                    }
+                    else {
+                        player.setVelocity(0, -100);
+                    }
+                    //set the direction the player walks
+                    player.setDirection(PlayerSprite.Direction.WALK_UP);
+                }
+                
+                //check if the user wants to walk down.
+                if (input.contains("DOWN")) {
+                    //check if the user walks into a world boundary
+                    if (player.intersects_bottom(worldBoundBottom)) {
+                        //Reset the velocity
+                        player.setVelocity(0, 0);
+                        //check if the player walks into a sprite
+                    }
+                    else {
+                        player.setVelocity(0, 100);
+                    }
+                    //set the direction the player walks
+                    player.setDirection(PlayerSprite.Direction.WALK_DOWN);
+                }
+                
+                if (menu_input.contains("E")) {
+                    if (player.intersect(dungeon_background_sprites.get(1))) {
+                        for (String s : game.goTo(new Command(CommandWord.GO, "haystack"))) {
+                            infobox.appendText("\n" + s + "\n");
+                        }
+                        playerinventory.update(game);
+                    }
+                    menu_input.remove("E");
+                }
+                //update the players velocity
+                player.update(elapsedTime);
+                //clear our player
+                moveable_gc.clearRect(0, 0, 1024, 512);
+                //render our new player
+                player.render(moveable_gc);
+
+                //check if the user wants to see a menu.
+                if (menu_input.contains("I")) {
+                    if (!playerinventory.isShown()) {
+                        root.getChildren().add(menu);
+                        playerinventory.setShown(true);
+                    }
+                } else if (playerinventory.isShown()) {
+                    root.getChildren().remove(menu);
+                    playerinventory.setShown(false);
+                }
+            }
+                    
+            public void setNewScene() {
+                switch (game.getCurrentRoomId()) {
+                    case 12:
+                        WizardOfTreldan.setGruulsLairScene();
+                        break;
+                    
+                }
+            }
+        }.start();
         
         return theScene;
     }
--- zuul-framework/src/Gui2D/SpriteController/Maps/Dungeon_sprites.java
+++ zuul-framework/src/Gui2D/SpriteController/Maps/Dungeon_sprites.java
@@ -20,11 +20,13 @@ import java.util.List;
 public class Dungeon_sprites {
     private SpriteController world;
     
-    private List<Sprite> spriteList;
+    private List<Sprite> background_sprites;
+    private List<Sprite> foreground_sprites;
     
     public Dungeon_sprites(SpriteController world){
         this.world = world;
-        this.spriteList = new ArrayList();
+        this.background_sprites = new ArrayList();
+        this.foreground_sprites = new ArrayList();
     }
     
     public void setDungeon_background_SingleSprites(){
@@ -39,7 +41,7 @@ public class Dungeon_sprites {
         Sprite dungeonTileset = new Groupsprite_random(dungeonTilesetList);
         dungeonTileset.setPosition(157,120);
         dungeonTileset.setSize(710, 304);
-        spriteList.add(dungeonTileset);
+        background_sprites.add(dungeonTileset);
         
         // END OF TILESET
         
@@ -57,7 +59,7 @@ public class Dungeon_sprites {
         Sprite dungeonWALLCircle = new Groupsprite_circle(dungeonWALLList);
         dungeonWALLCircle.setPosition(125, 60);
         dungeonWALLCircle.setSize(768, 384);
-        spriteList.add(dungeonWALLCircle);
+        background_sprites.add(dungeonWALLCircle);
         
         // END OF WALL
         
@@ -66,7 +68,7 @@ public class Dungeon_sprites {
         Sprite libaryDoor = world.getMisc_sprites().getDoor_metal();
         libaryDoor.setPosition(700, 75);
         libaryDoor.setSize(32, 48);
-        spriteList.add(libaryDoor);
+        background_sprites.add(libaryDoor);
         
         // END OF LIBARY ENTRANCE
         
@@ -75,32 +77,32 @@ public class Dungeon_sprites {
         Sprite torch1 = world.getMisc_sprites().getTorch_wall();
         torch1.setPosition(670, 80);
         torch1.setSize(16, 38);
-        spriteList.add(torch1);
+        background_sprites.add(torch1);
         
         Sprite torch2 = world.getMisc_sprites().getTorch_wall();
         torch2.setPosition(746, 80);
         torch2.setSize(16, 38);
-        spriteList.add(torch2);
+        background_sprites.add(torch2);
         
         Sprite hangingSkeleton1 = world.getMisc_sprites().getSkeleton_hanging();
         hangingSkeleton1.setPosition(500, 80);
         hangingSkeleton1.setSize(32, 45);
-        spriteList.add(hangingSkeleton1);
+        background_sprites.add(hangingSkeleton1);
         
         Sprite hangingSkeleton2 = world.getMisc_sprites().getSkeleton_hanging();
         hangingSkeleton2.setPosition(400, 80);
         hangingSkeleton2.setSize(32, 45);
-        spriteList.add(hangingSkeleton2);
+        background_sprites.add(hangingSkeleton2);
         
         Sprite hangingSkeleton3 = world.getMisc_sprites().getSkeleton_hanging();
         hangingSkeleton3.setPosition(300, 80);
         hangingSkeleton3.setSize(32, 45);
-        spriteList.add(hangingSkeleton3);
+        background_sprites.add(hangingSkeleton3);
         
         Sprite hangingSkeleton4 = world.getMisc_sprites().getSkeleton_hanging();
         hangingSkeleton4.setPosition(200, 80);
         hangingSkeleton4.setSize(32, 45);
-        spriteList.add(hangingSkeleton4);
+        background_sprites.add(hangingSkeleton4);
         
         List<SingleSprite> bonePileList1 = new ArrayList();
         bonePileList1.add(world.getMisc_sprites().getSkeleton_bones_pile());
@@ -108,32 +110,32 @@ public class Dungeon_sprites {
         Sprite bonePileRow = new Groupsprite_random(bonePileList1);
         bonePileRow.setSize(128, 128);
         bonePileRow.setPosition(160, 290);
-        spriteList.add(bonePileRow);
+        background_sprites.add(bonePileRow);
         
         Sprite SpiderWebUPPERLEFT1 = world.getMisc_sprites().getSpiderWeb_UPPER_LEFT();
         SpiderWebUPPERLEFT1.setSize(32, 23);
         SpiderWebUPPERLEFT1.setPosition(156, 122);
-        spriteList.add(SpiderWebUPPERLEFT1);
+        background_sprites.add(SpiderWebUPPERLEFT1);
         
         Sprite SpiderWebLOWERRIGHT1 = world.getMisc_sprites().getSpiderWeb_LOWER_RIGHT();
         SpiderWebLOWERRIGHT1.setSize(24, 32);
         SpiderWebLOWERRIGHT1.setPosition(838, 389);
-        spriteList.add(SpiderWebLOWERRIGHT1);
+        background_sprites.add(SpiderWebLOWERRIGHT1);
         
         Sprite cellWall1 = world.getMisc_sprites().getCell_ironbar_straight();
         cellWall1.setSize(10, 100);
         cellWall1.setPosition(265, 73);
-        spriteList.add(cellWall1);
+        background_sprites.add(cellWall1);
         
         Sprite cellWall2 = world.getMisc_sprites().getCell_ironbar_straight();
         cellWall2.setSize(10, 100);
         cellWall2.setPosition(365, 73);
-        spriteList.add(cellWall2);
+        background_sprites.add(cellWall2);
         
         Sprite cellWall3 = world.getMisc_sprites().getCell_ironbar_straight();
         cellWall3.setSize(10, 100);
         cellWall3.setPosition(460, 73);
-        spriteList.add(cellWall3);
+        background_sprites.add(cellWall3);
         
         // END OF MISC
         
@@ -154,9 +156,9 @@ public class Dungeon_sprites {
         skeleton3.setSize(30, 47);
         skeleton3.setPosition(450, 250);
         
-        spriteList.add(skeleton1);
-        spriteList.add(skeleton2);
-        spriteList.add(skeleton3);
+        background_sprites.add(skeleton1);
+        background_sprites.add(skeleton2);
+        background_sprites.add(skeleton3);
         
         enemies.add((SingleSprite)skeleton1);
         enemies.add((SingleSprite)skeleton2);
@@ -170,7 +172,10 @@ public class Dungeon_sprites {
      * 
      * @return 
      */
-    public List<Sprite> getSpriteList() {
-        return spriteList;
+    public List<Sprite> getDungeon_background_sprites() {
+        return background_sprites;
+    }
+    public List<Sprite> getDungeon_foreground_sprites() {
+        return foreground_sprites;
     }
 }
