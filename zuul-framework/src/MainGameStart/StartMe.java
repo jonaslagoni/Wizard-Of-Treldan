81ab6870bdde56ad9d00ecd3fa4ee6d36d62d695
jonaslagoni <jonas-lt@live.dk>
2016-12-01 09:47:48 +0100

    comments for main program

--- zuul-framework/src/MainGameStart/StartMe.java
+++ zuul-framework/src/MainGameStart/StartMe.java
@@ -23,34 +23,48 @@ public class StartMe extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        
+        //create a new button for picking the 2d gui game
         Button Gui2D = new Button();
+        //set the size of the button
         Gui2D.setMinSize(300, 500);
+        //set the background of the button
         Gui2D.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_GUI2D.png')");
+        //set an action when the button is clicked
         Gui2D.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                //close this stage
                 primaryStage.close();
+                //open the 2d gui
                 new WizardOfTreldan().start(new Stage());
             }
         });
+        //set a new action when the mouse enteres the button
         Gui2D.setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                //set the background of the button
                 Gui2D.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_GUI2D_hover.png')");
             }
         });
+        //set a new action when the mouse exits the button
         Gui2D.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                //set the background of the button
                 Gui2D.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_GUI2D.png')");
             }
         });
         
         
-        
+        //create a new button for picking the text based gui game
         Button GuiText = new Button();
+        //set the size of the button
         GuiText.setMinSize(300, 500);
+        //set the background of the button
         GuiText.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_text.png')");
+        //set an action when the button is clicked
         GuiText.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -58,25 +72,33 @@ public class StartMe extends Application {
                 new WizardOfTreldan().start(new Stage());
             }
         });
+        //set a new action when the mouse enteres the button
         GuiText.setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                //set the background of the button
                 GuiText.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_text_hover.png')");
             }
         });
+        //set a new action when the mouse exits the button
         GuiText.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                //set the background of the button
                 GuiText.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_text.png')");
             }
         });
+        //relocate the button
         GuiText.relocate(300, 0);
         
         
-        
+        //create a new button for picking the button based gui game
         Button GuiButtons = new Button();
+        //set the size of the button
         GuiButtons.setMinSize(300, 500);
+        //set the background of the button
         GuiButtons.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_buttons.png')");
+        //set an action when the button is clicked
         GuiButtons.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -84,36 +106,46 @@ public class StartMe extends Application {
                 new WizardOfTreldan().start(new Stage());
             }
         });
+        //set a new action when the mouse enteres the button
         GuiButtons.setOnMouseEntered(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                //set the background of the button
                 GuiButtons.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_buttons_hover.png')");
             }
         });
+        //set a new action when the mouse exits the button
         GuiButtons.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                //set the background of the button
                 GuiButtons.setStyle("-fx-background-image: url('/GuiSelection/buttonBackground_buttons.png')");
             }
         });
+        //relocate the button
         GuiButtons.relocate(600, 0);
         
         
         
         
-        
+        //create a new group object to hold all our buttons
         Group root = new Group();
+        //add all the buttons
         root.getChildren().add(Gui2D);
         root.getChildren().add(GuiText);
         root.getChildren().add(GuiButtons);
         
+        //Create a new Scene object from our group of buttons
         Scene scene = new Scene(root, 900, 539);
         
-        
+        //set the title of the stage
         primaryStage.setTitle("Wizard of Treldan");
+        //set the size of the stage
         primaryStage.setWidth(900);
         primaryStage.setHeight(539);
+        //Set the scene
         primaryStage.setScene(scene);
+        //show the window
         primaryStage.show();
     }
 
